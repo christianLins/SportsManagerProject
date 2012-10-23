16f08609c8c223b23f0ea707d53b29e6eea7384d
markusmo <markus.mo@gmx.net>
2012-10-23 13:02:11 +0200

    first version of a facade

--- SportsClubManager/src/sportsclubmanager/domain/DomainFacade.java
+++ SportsClubManager/src/sportsclubmanager/domain/DomainFacade.java
@@ -11,6 +11,13 @@ import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import sportsclubmanager.domain.classes.Competition;
 import sportsclubmanager.domain.classes.Country;
+import sportsclubmanager.domain.classes.Department;
+import sportsclubmanager.domain.classes.DepartmentTypeOfSport;
+import sportsclubmanager.domain.classes.League;
+import sportsclubmanager.domain.classes.Match;
+import sportsclubmanager.domain.classes.Member1;
+import sportsclubmanager.domain.classes.Team;
+import sportsclubmanager.domain.classes.TypeOfSport;
 import sportsclubmanager.utils.HibernateUtil;
 
 /**
@@ -19,31 +26,14 @@ import sportsclubmanager.utils.HibernateUtil;
  */
 public class DomainFacade
 {
-    public ArrayList<Country> getAllCountries()
-    {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        ArrayList<Country> allCountries = (ArrayList<Country>) session.createCriteria(Country.class).list();
-        return allCountries;
-    }
     
-    public Country getCountryByName(String name)
-    {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Country country = (Country) session.createCriteria(Country.class).add(Restrictions.eq("name", name)).uniqueResult();
-        return country;
-    }
-    
-    public ArrayList<Competition> getAllCompetitions()
-    {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        ArrayList<Competition> allCompetitions = (ArrayList<Competition>) session.createCriteria(Competition.class).list();
-        return allCompetitions;
-    }
-    
-    public ArrayList<Competition> getCompetitionsByDate(Date from, Date to)
+    /**
+     * returns all competitions in a time span
+     * @param from start of time span
+     * @param to end of time span
+     * @return all competitions between a timespan given
+     */
+    public static ArrayList<Competition> getCompetitionsByDate(Date from, Date to)
     {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
@@ -53,5 +43,73 @@ public class DomainFacade
         return (ArrayList<Competition>) query.list();
     }
     
-    //You guys can do the rest :-D
+    /**
+     * returns a the department of a type of sport
+     * @param sport the sport the department belongs to
+     * @return the department of the sport
+     */
+    public static Department getDepartmentsBySport(TypeOfSport sport)
+    {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from DepartmentTypeOfSport where typeOfSport = :sport");
+        query.setParameter("sport", sport);
+        DepartmentTypeOfSport temp = (DepartmentTypeOfSport) query.uniqueResult();
+        return temp.getDepartmentidDepartment();
+    }
+    
+    /**
+     * returns all matches of a competition
+     * @param competition the competition the matches are in
+     * @return all matches of the competition given
+     */
+    public static ArrayList<Match> getMatchesByCompetition(Competition competition)
+    {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Match where competition = :competition");
+        query.setParameter("competition", competition);
+        return (ArrayList<Match>) query.list();
+    }
+    
+    /**
+     * returns a member with the firstname and lastname
+     * @param firstname the firstname of the member
+     * @param lastname the lastname of the member
+     * @return a member with the firstname and lastname given
+     */
+    public static Member1 getMemberByName(String firstname, String lastname)
+    {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Member1 where prename = :firstname and lastname = :lastname");
+        return (Member1) query.uniqueResult();
+    }
+    
+    /**
+     * returns a object out of the database by its class with a specific name
+     * @param <T> the type of class
+     * @param clazz the class
+     * @param name name of the object
+     * @return a object with a name given
+     */
+    public static <T> T getByName(Class<T> clazz, String name)
+    {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        return (T) session.createCriteria(clazz).add(Restrictions.eq("name", name)).uniqueResult();
+    }
+    
+    /**
+     * returns all database entries of a table by its class
+     * @param <T> the type of class
+     * @param clazz the class
+     * @return all database entries of a table
+     */
+    public static <T> T getAll(Class<T> clazz)
+    {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        return (T) session.createCriteria(clazz).list();
+    }
 }
