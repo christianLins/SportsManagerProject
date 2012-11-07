49012088ce864411a3caa7a1c488c61267d57e98
Thomas Schwarz <thomas.schwarz@students.fhv.at>
2012-11-07 19:15:04 +0100

    id not found exception

--- DTO/src/dto/mapper/contract/IdNotFoundException.java
+++ DTO/src/dto/mapper/contract/IdNotFoundException.java
@@ -2,17 +2,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package dto.controller.contract;
+package dto.mapper.contract;
 
 /**
 
  @author Thomas
  */
-public class IdNotFoundException extends Exception
+public class IdNotFoundException
+        extends Exception
 {
-
     public IdNotFoundException()
     {
     }
-    
 }
