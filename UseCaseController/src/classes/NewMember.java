fc493c53637bbf44f652996e4d45f45ce7d5270d
Gregotsch <dominik.gregotsch@students.fhv.at>
2012-11-08 12:05:56 +0100

    zwischencommit

---
+++ UseCaseController/src/classes/NewMember.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+import contract.*;
+import dto.contract.*;
+import java.util.List;
+/**
+ *
+ * @author EnjoX
+ */
+public class NewMember implements INewMember{
+
+    @Override
+    public void setNewMember(IMember member, IAddress address) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<IDepartment> getDepartments() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<IClubTeam> getClubTeams(List<Integer> clubTeams) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setNewMember(IMember member, IAddress address, IDepartment department, IClubTeam clubTeam, IRole role) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
