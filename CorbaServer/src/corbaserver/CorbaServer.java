d793b9db50d9e579b24c5f197b80789bd0108cc9
Thomas Schwarz <thomas.schwarz@students.fhv.at>
2012-11-16 16:14:09 +0100

    corbaserver

--- CorbaServer/src/corbaserver/CorbaServer.java
+++ CorbaServer/src/corbaserver/CorbaServer.java
@@ -4,17 +4,81 @@
  */
 package corbaserver;
 
-/**
+import corbaContract.generated.*;
+import java.util.logging.*;
+import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.*;
+import org.omg.CosNaming.NamingContextPackage.*;
+import org.omg.PortableServer.*;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAPackage.*;
 
+/**
  @author Thomas
  */
 public class CorbaServer
+        implements Runnable
 {
     /**
      @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        new Thread(new CorbaServer()).start();
+    }
+
+    @Override
+    public void run()
+    {
+        try
+        {
+            String[] args1 = new String[]
+            {
+                "-ORBInitialPort", "2050"
+            };
+
+            ORB orb = ORB.init(args1, null);
+            POA rootpoa = POAHelper.narrow(orb.resolve_initial_references("RootPOA"));
+            rootpoa.the_POAManager().activate();
+            MatchresultDataproviderServant helloServant = new MatchresultDataproviderServant();
+            helloServant.setORB(orb);
+            org.omg.CORBA.Object ref = rootpoa.servant_to_reference(helloServant);
+            MatchresultDataprovider href = MatchresultDataproviderHelper.narrow(ref);
+            org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
+            NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+            NameComponent cosName[] = ncRef.to_name("HelloObject");
+            ncRef.rebind(cosName, href);
+            System.out.println("Corba Server ready and waiting");
+            orb.run();
+        }
+        catch (NotFound ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (CannotProceed ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (org.omg.CosNaming.NamingContextPackage.InvalidName ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (ServantNotActive ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (WrongPolicy ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (AdapterInactive ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (InvalidName ex)
+        {
+            Logger.getLogger(CorbaServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
