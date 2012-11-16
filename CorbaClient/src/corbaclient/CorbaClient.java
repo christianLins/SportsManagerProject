25da264647f627996382ad6de7b2b197c7f512c4
Thomas Schwarz <thomas.schwarz@students.fhv.at>
2012-11-16 15:36:41 +0100

    corba

---
+++ CorbaClient/src/corbaclient/CorbaClient.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corbaclient;
+
+/**
+
+ @author Thomas
+ */
+public class CorbaClient
+{
+    /**
+     @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
